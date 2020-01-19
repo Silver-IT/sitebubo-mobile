598c337996c1b27d9fa455c69e8fa8e2a6b28868
BeeyondApp <49462201+BeeyondApp@users.noreply.github.com>
2020-01-20 01:04:20 +1000

    modified auth-api for initial-build

--- src/app/apis/auth/auth-api.service.ts
+++ src/app/apis/auth/auth-api.service.ts
@@ -25,7 +25,6 @@ export class AuthApiService extends BaseApiService {
     let url = this.auth_url + 'login?';
     url += 'email=' + email + '&password=' + password;
     url += '&device_id=' + deviceID + '&device_name=' + deviceName;
-    console.log(url);
     return this.sendGetRequest(url);
   }
 
@@ -38,7 +37,6 @@ export class AuthApiService extends BaseApiService {
   facebookLogIn(email, deviceID): any {
     let url = this.auth_url + 'login?';
     url += 'email=' + email + '&openid=facebook&device_id=' + deviceID;
-    console.log(url);
     return this.sendGetRequest(url);
   }
 
@@ -46,14 +44,12 @@ export class AuthApiService extends BaseApiService {
     let url = this.auth_url + 'register?';
     url += 'email=' + email + '&name=' + name + '&mobile=000&address=&user_type=Member&status=1&openid=facebook&device_id=' + deviceID;
     // alert('fb sign up' + url);
-    console.log(url);
     return this.sendGetRequest(url);
   }
 
   resendEmail(userID): any {
     let url = this.auth_url + 'resendverification?';
     url += 'user_id=' + userID;
-    console.log(url);
     return this.sendGetRequest(url);
   }
 
@@ -65,35 +61,30 @@ export class AuthApiService extends BaseApiService {
     url += '&name=' + fullname;
     url += '&mobile=000&address=&user_type=Member&status=1';
     url += '&device_id=' + deviceID;
-    console.log(url);
     return this.sendGetRequest(url);
   }
 
   validateEmail(userID, code): any {
     let url = this.auth_url + 'validateemail?';
     url += 'code=' + code + '&user_id=' + userID;
-    console.log(url);
     return this.sendGetRequest(url);
   }
 
   forgotPassword(email) {
     let url = this.auth_url + 'forgotPassword?';
     url += 'email=' + email;
-    console.log(url);
     return this.sendGetRequest(url);
   }
 
   resetPassword(email, password, code): any {
     let url = this.auth_url + 'resetpassword?';
     url += 'email=' + email + '&password=' + password + '&code=' + code;
-    console.log(url);
     return this.sendGetRequest(url);
   }
 
   verifyToken(token): any {
     let url = this.auth_url + 'verifytoken?';
     url += 'token=' + token;
-    console.log(url);
     return this.sendGetRequest(url);
   }
 }
