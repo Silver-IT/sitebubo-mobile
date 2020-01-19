5f5edb30a32cdda78df41c15c967b0d737fc363d
BeeyondApp <49462201+BeeyondApp@users.noreply.github.com>
2020-01-19 23:55:58 +1000

    Version 1 build for android platform

--- src/app/pipes/datetime.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DatetimePipe } from './datetime.pipe';
-
-describe('DatetimePipe', () => {
-  it('create an instance', () => {
-    const pipe = new DatetimePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
